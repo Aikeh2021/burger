83b790d094ac31d2fa8cb55969f5ccc3a0e19c03
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-03 15:53:19 -0500

    Started scaffolding out some pseudo code in the schema.sql file.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Dropping the database if it already exists --
+
+-- Creating the database --
+
+-- Using the burgers database --
+
+-- Creating the burgers table --
+
+-- Creating the burgers table columns --
+
+-- id column that auto increments --
+
+-- burger_name column of string type --
+
+-- devoured column of boolean (probably set it to false to start out with) --
+
