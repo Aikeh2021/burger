7b60dcfc22efdb64243e2a699197a799a4303702
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-03 16:11:07 -0500

    Filled in the sql commands for the pseudocode on the schema.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,18 @@
 -- Dropping the database if it already exists --
-
+DROP DATABASE IF EXISTS burgers_db;
 -- Creating the database --
-
+CREATE DATABASE burgers_db;
 -- Using the burgers database --
-
+USE burgers_db;
 -- Creating the burgers table --
-
--- Creating the burgers table columns --
-
+CREATE TABLE burgers (
+-- Writing out the burgers table columns --
 -- id column that auto increments --
-
+id INTEGER AUTO_INCREMENT NOT NULL,
 -- burger_name column of string type --
-
+burger_name VARCHAR(50) NOT NULL,
 -- devoured column of boolean (probably set it to false to start out with) --
-
+devoured BOOLEAN DEFAULT(false),
+-- Setting the id to the primary key
+PRIMARY KEY (id)
+);
