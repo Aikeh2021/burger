9085b28530588f1d65f4b290fe5f6bbb1e065c71
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-03 16:25:12 -0500

    Scaffolded out my basic server and the command to start up the sql commands in sequelize when the app begins. Added a test route as well to ensure the server is making url requests correctly.

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Test Route
+app.get("/api/config", (req, res) => {
+    res.json(
+        {
+            isWorking : "Success! I'm working."
+        }
+    )
+});
+
+// db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on http://localhost:${PORT}`);
+    });
+  });
